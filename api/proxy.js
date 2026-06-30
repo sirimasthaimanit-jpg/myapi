017202ec7d434e7d57a517fbc8f7c588d56b22f1
sirimasthaimanit-jpg <sirimasthaimanit@gmail.com>
2026-06-30 15:03:58 +0700

    Add files via upload

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,6 @@
-module.exports = async function handler(req, res) {
+const https = require('https');
+
+async function handler(req, res) {
   const targetUrl = new URL('https://tc.sbtc-mng.com/api.php');
 
   if (req.query && Object.keys(req.query).length > 0) {
@@ -17,20 +19,46 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    const upstreamResponse = await fetch(targetUrl, {
-      method: req.method || 'GET',
-      headers: {
-        'Accept': 'application/json, text/plain, */*'
-      }
+    const upstreamResponse = await new Promise((resolve, reject) => {
+      const request = https.request(
+        {
+          protocol: targetUrl.protocol,
+          hostname: targetUrl.hostname,
+          port: targetUrl.port || 443,
+          path: `${targetUrl.pathname}${targetUrl.search}`,
+          method: req.method || 'GET',
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'User-Agent': 'Vercel-Proxy/1.0'
+          },
+          rejectUnauthorized: false
+        },
+        (response) => {
+          let body = '';
+          response.setEncoding('utf8');
+          response.on('data', (chunk) => {
+            body += chunk;
+          });
+          response.on('end', () => {
+            resolve({
+              status: response.statusCode || 200,
+              headers: response.headers,
+              body
+            });
+          });
+        }
+      );
+
+      request.on('error', reject);
+      request.end();
     });
 
-    const body = await upstreamResponse.text();
-    const contentType = upstreamResponse.headers.get('content-type') || 'application/json; charset=utf-8';
+    const contentType = upstreamResponse.headers['content-type'] || 'application/json; charset=utf-8';
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', contentType);
     res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-    res.status(upstreamResponse.status).send(body);
+    res.status(upstreamResponse.status).send(upstreamResponse.body);
   } catch (error) {
     console.error('Proxy error:', error);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -39,4 +67,7 @@ module.exports = async function handler(req, res) {
       message: 'Unable to reach upstream API'
     });
   }
-};
+}
+
+module.exports = handler;
+module.exports.default = handler;
